e9618664cc2ae64a1bc99bd5461044a54ff3fa15
Sander Pick <sanderpick@gmail.com>
2015-03-03 18:41:28 -0500

    remove logging

--- start.js
+++ start.js
@@ -13,7 +13,6 @@ var config = {};
 _.each(require('./config.json'), function (v, k) {
   config[k] = process.env[k] || v;
 });
-console.log(config.INSTAGRAM_USER_CALLBACK_URL)
 
 Instagram.set('client_id', config.INSTAGRAM_CLIENT_ID);
 Instagram.set('client_secret', config.INSTAGRAM_CLIENT_SECRET);
