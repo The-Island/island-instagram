774bdad983a4279832d9510fd425b1a8e86c4b01
Sander Pick <sanderpick@gmail.com>
2015-03-03 16:02:01 -0500

    use instagram lib

--- start.js
+++ start.js
@@ -12,8 +12,6 @@ var Instagram = require('instagram-node-lib');
 
 Instagram.set('client_id', config.INSTAGRAM_CLIENT_ID);
 Instagram.set('client_secret', config.INSTAGRAM_CLIENT_SECRET);
-Instagram.set('callback_url', config.INSTAGRAM_CALLBACK_URL);
-Instagram.set('verify_token', config.INSTAGRAM_VERIFY_TOKEN);
 
 // Start a worker.
 worker.start({
@@ -21,7 +19,23 @@ worker.start({
   onReady: function () {
 
     // Subscribe to authorized users' media.
-    Instagram.subscriptions.subscribe({object: 'user', aspect: 'media'});
+    Instagram.subscriptions.subscribe({
+      object: 'user',
+      aspect: 'media',
+      callback_url: config.INSTAGRAM_USER_CALLBACK_URL,
+      verify_token: config.INSTAGRAM_VERIFY_TOKEN
+    });
+
+    // Subscribe to our tags.
+    // _.each(config.INSTAGRAM_TAGS, function (tag) {
+    //   Instagram.subscriptions.subscribe({
+    //     object: 'tag',
+    //     object_id: tag,
+    //     aspect: 'media',
+    //     callback_url: config.INSTAGRAM_TAG_CALLBACK_URL,
+    //     verify_token: config.INSTAGRAM_VERIFY_TOKEN
+    //   });
+    // });
 
     util.log('Ready for instagrams.');
   },
